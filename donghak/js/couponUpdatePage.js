c41df1907f5e4c0ce4c492927099879b1de9a013
이동학 <donghak@idonghag-ui-MacBookPro-2.local>
2024-06-07 16:08:59 +0900

    feat: ownerMainPage couponUpdatePage

--- donghak/js/couponUpdatePage.js
+++ donghak/js/couponUpdatePage.js
@@ -19,3 +19,24 @@ modalClose[0].addEventListener("click", function() {
 modalClose[1].addEventListener("click", function() {
 	document.querySelector('.informUpdateModal').style.display = "none";
 });
+
+const storeUpdateButton = document.getElementById('storeUpdateBtn');
+const serviceUpdateButton = document.getElementById('serviceUpdateBtn');
+
+storeUpdateButton.addEventListener('click', (event) => {
+		event.preventDefault(); // 폼의 기본 제출 동작을 막음
+	
+	const confirmApproval = confirm("정말 수정하시겠습니까?");
+	if (confirmApproval) {
+		event.target.closest('form').submit();
+	}
+});
+
+serviceUpdateButton.addEventListener('click', (event) => {
+	event.preventDefault(); // 폼의 기본 제출 동작을 막음
+	
+	const confirmApproval = confirm("정말 수정하시겠습니까?");
+	if (confirmApproval) {
+		event.target.closest('form').submit();
+	}
+});
